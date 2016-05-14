63c99e542e2e77f1420c3c0c8f3312fa087817d4
Eric J. Ma <ericmajinglong@gmail.com>
2016-05-14 19:18:39 -0400

    changed name to pyflattener

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(name='flattener',
+setup(name='pyflatten',
       version='1.0',
       description='A utility for flattening nested data structures.',
       author='Eric J. Ma',
       author_email='ericmajinglong@gmail.com',
-      url='https://github.com/ericmjl/flattener',
+      url='https://github.com/ericmjl/pyflatten',
       )
