f20bbc4a009e9daf15b6406321bbcdec3f026f34
Eric Ma <ericmajinglong@gmail.com>
2018-02-23 17:51:34 -0500

    Made package work with setup script.

--- __init__.py
+++
@@ -1,1 +0,0 @@
-from .pyflatten import flatten
--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup
 
 setup(name='pyflatten',
-      version='1.0',
+      version='1.0.1',
       description='A utility for flattening nested data structures.',
       author='Eric J. Ma',
       author_email='ericmajinglong@gmail.com',
       url='https://github.com/ericmjl/pyflatten',
+      license='MIT',
+      packages=['pyflatten'],
       )
